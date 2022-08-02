efd89fdcc1afccf420ca3a0fec9d5abccf3948f4
Nahom Habtamu <nhabtamu42@gmail.com>
2022-08-03 02:44:03 +0300

    added some configurations

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express()
 
 const port = process.env.PORT || 5000
 
-app.listen(process.env.PORT||5000,()=> {
+app.listen(port, () => {
     console.log("App running on http://localhost:" + port)
 })
