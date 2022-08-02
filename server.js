aeee4e848c2f354c64f5e02089deaddd8cc70ac8
Nahom Habtamu <nhabtamu42@gmail.com>
2022-08-03 02:39:40 +0300

    added some configuration

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
 
 const app = express()
 
+const port = process.env.PORT || 5000
+
 app.listen(process.env.PORT||5000,()=> {
-    console.log("App running on port 5000")
+    console.log("App running on http://localhost:" + port)
 })
