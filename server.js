42de5e5d48a79e51b03fcfedcdbba09a66186e82
Nahom Habtamu <nhabtamu42@gmail.com>
2022-08-07 01:07:05 +0300

    db connection and authentication

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 
+const db = require('./backend/config/database')
+const models = require('./backend/model/index')
+
 const app = express()
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-    console.log("App running on http://localhost:" + port)
+//Test database connection
+db.authenticate()
+.then(() => console.log('DATABASE CONNECTED SUCCESSFULLY'))
+.catch(err => console.log(err))
+
+db.sync({force: true})
+.then(() => {
+    app.listen(port, console.log("App running on http://localhost:" + port))
 })
+.catch(err => console.log('sync err: ', err))
