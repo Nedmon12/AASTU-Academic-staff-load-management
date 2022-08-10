235371bd8406b60566a5e1cdc8c41a5ed34b8c0f
Nahom Habtamu <nhabtamu42@gmail.com>
2022-08-10 12:35:18 +0300

    route files created

---
+++ backend/routes/courseRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+//const courseController = require('../controllers/userController')
+
+// router.post('/login',userController.login)
+// router.post('/register', userController.registerUser)
+// router.get('/logout', userController.logout)
+
+module.exports = router
