bcec483b1a060d400a1b737866fd7d0180e8ef3a
nati589 <natimok55@gmail.com>
2022-08-24 23:17:47 -0900

    changes

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,13 +1,35 @@
-import { Button, Card } from '@mui/material';
+import { Button, Card, FormControl, FormHelperText, Input, InputLabel, Paper, TextField } from '@mui/material';
 import React from 'react'
 import { styled } from '@mui/material/styles';
 import { Box } from '@mui/system';
 
-
+const MyField = styled(TextField)(({theme}) => ({
+  // width: '100%',
+  margin: theme.spacing(2),
+  // padding: theme.spacing(2)
+}))
 
 const Login = () => {
   return (
-        <Box>
+        <Box sx={{
+          width: '30%',
+          // height: '100vh',
+          margin: 'auto',
+          marginTop: '150px'
+          // padding: '20px',
+          // display: 'flex', 
+          // alignItems: 'center'
+          // marginTop: 'auto'
+        }}>
+          <Paper>
+            <FormControl color='error'>
+              <MyField
+                label='smth'
+                value={'hello'}
+              > 
+              </MyField>
+            </FormControl>
+          </Paper>
         </Box>
   )
 }
